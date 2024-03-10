4e3a2c0689075df6d6197492b3d896cf2f09434c
AloizioMacedo <atsam2@hotmail.com>
2024-03-10 18:56:11 +0000

    Make first attempt towards synchronizing

--- src/main.rs
+++ src/main.rs
@@ -6,36 +6,45 @@ use esp_idf_svc::hal::peripherals::Peripherals;
 // Wrapped up by main, avoiding the boilerplate of link_patches and initialize_default.
 fn main_logic() -> Result<()> {
     let stages = [6, 2, 8]; // Seconds.
+    let sum_stages: u64 = stages.iter().sum();
+
+    let offset = 0;
+
     let peripherals = Peripherals::take()?;
+
     let mut led1 = PinDriver::output(peripherals.pins.gpio16)?;
     let mut led2 = PinDriver::output(peripherals.pins.gpio4)?;
     let mut led3 = PinDriver::output(peripherals.pins.gpio0)?;
 
-    for (i, dur) in stages.iter().enumerate().cycle() {
-        if i == 0 {
+    loop {
+        let now = std::time::SystemTime::now().duration_since(std::time::SystemTime::UNIX_EPOCH)?;
+
+        log::info!("Now: {:?}", std::time::SystemTime::now());
+        log::info!("Unix Epoch: {:?}", std::time::SystemTime::UNIX_EPOCH);
+        log::info!("Duration since: {:?}", now);
+
+        if ((now.as_secs() + offset) % sum_stages) < 6 {
             _ = led2.set_low();
             _ = led3.set_low();
             _ = led1.set_high();
 
-            log::info!("red");
-        } else if i == 1 {
+            log::info!("red")
+        } else if ((now.as_secs() + offset) % sum_stages) < 8 {
             _ = led1.set_low();
             _ = led3.set_low();
             _ = led2.set_high();
 
-            log::info!("yellow");
-        } else {
+            log::info!("yellow")
+        } else if ((now.as_secs() + offset) % sum_stages) < 16 {
             _ = led1.set_low();
             _ = led2.set_low();
             _ = led3.set_high();
 
-            log::info!("green");
+            log::info!("green")
         }
 
-        FreeRtos::delay_ms(dur * 1000);
+        FreeRtos::delay_ms(500);
     }
-
-    Ok(())
 }
 
 fn main() -> Result<()> {
